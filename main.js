3af14e210c933dc6c3a5113e7facab831b1a9fae
wodzynski <macko5395@gmail.com>
2018-06-06 14:05:58 +0200

    Hide Those Folks 1.0

---
+++ main.js
@@ -0,0 +1,6 @@
+function remove() {
+    document.querySelector("#BuddylistPagelet").style.display = "none";
+    document.querySelector("#pagelet_sidebar").style.display = "none";
+}
+
+window.onload = remove;
